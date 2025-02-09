cb4ec6f51a43e3fcc042186bcd161ed308caf6c8
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-02-09 06:49:17 -0300

    adiciona mais informacoes no df gerado

--- data-raw/gerar_df.R
+++ data-raw/gerar_df.R
@@ -12,12 +12,23 @@ questoes <- lista_json |>
   purrr::list_rbind() |>
   dplyr::filter(validado %in% c("TRUE", TRUE)) |>
   dplyr::mutate(
+    url_github_base = glue::glue(
+      "https://github.com/beatrizmilz/BancoDeQuestoes/blob/main/data-raw/questoes/{stringr::str_to_lower(vestibular)}/{ano}"
+    ),
     url_github = glue::glue(
-      "https://github.com/beatrizmilz/BancoDeQuestoes/blob/main/data-raw/questoes/{stringr::str_to_lower(vestibular)}/{ano}/{questao_numero}.json"
+      "{url_github_base}/{questao_numero}.json"
     )
   )
 
 questoes
 
+provas <- questoes |>
+  dplyr::distinct(vestibular, ano, prova, url_github_base) |>
+  dplyr::mutate(
+    url_pdf_prova = glue::glue("{url_github_base}/prova.pdf"),
+    url_pdf_gabarito = glue::glue("{url_github_base}/gabarito.pdf"
+  ))
+
 
 usethis::use_data(questoes, overwrite = TRUE)
+usethis::use_data(provas, overwrite = TRUE)
