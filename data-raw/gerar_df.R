69bf2ef1bcd8c3bd56ee5db525698552f047fca6
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-02-08 21:33:00 -0300

    apenas manter os arquivos validados

--- data-raw/gerar_df.R
+++ data-raw/gerar_df.R
@@ -1,12 +1,18 @@
-questoes_json <- list.files("data-raw/questoes",
+questoes_json <- list.files(
+  "data-raw/questoes",
   full.names = TRUE,
-  recursive = TRUE, pattern = "\\.json$"
+  recursive = TRUE,
+  pattern = "\\.json$"
 )
 
 lista_json <- purrr::map(questoes_json, jsonlite::fromJSON)
 
 questoes <- lista_json |>
   purrr::map(tibble::as_tibble) |>
-  purrr::list_rbind()
+  purrr::list_rbind() |>
+  dplyr::filter(validado %in% c("TRUE", TRUE))
+
+questoes
+
 
 usethis::use_data(questoes, overwrite = TRUE)
